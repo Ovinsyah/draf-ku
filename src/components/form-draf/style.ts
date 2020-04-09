19ad4006a046b3d1cc6b7bae050b6dc7261fda87
ovinsyah <ovinsyah.bayhaqy@ottencoffee.co.id>
2020-04-09 10:22:03 +0700

    updated style modal

--- src/components/form-draf/style.ts
+++ src/components/form-draf/style.ts
@@ -4,7 +4,8 @@ export const Section = Styled.div`
   background: white;
   position: fixed;
   top: 0;
-  width: 450px;
+  max-width: 450px;
+  width: 100%;
   height: 100vh;
 `;
 export const Header = Styled.header`
